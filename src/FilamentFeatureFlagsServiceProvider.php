db70b3b87ae8efac36d7427ae3fee442a3257c81
Sebastiaan Kloos <sebastiaan@codecycler.com>
2022-04-29 11:46:42 +0200

    Added macros for Filament tables and forms

--- src/FilamentFeatureFlagsServiceProvider.php
+++ src/FilamentFeatureFlagsServiceProvider.php
@@ -3,7 +3,10 @@
 namespace RyanChandler\FilamentFeatureFlags;
 
 use Filament\PluginServiceProvider;
+use Filament\Tables\Columns\Column;
 use RyanChandler\FilamentFeatureFlags\Resources\FeatureFlagResource;
+use RyanChandler\LaravelFeatureFlags\Facades\Features;
+use Filament\Forms\Components\Component;
 
 class FilamentFeatureFlagsServiceProvider extends PluginServiceProvider
 {
@@ -12,4 +15,31 @@ class FilamentFeatureFlagsServiceProvider extends PluginServiceProvider
     protected array $resources = [
         FeatureFlagResource::class,
     ];
+
+    protected function registerMacros(): void
+    {
+        Component::macro('featureEnabled', function (string $featureFlag): static {
+            $this->isVisible = Features::enabled($featureFlag);
+
+            return $this;
+        });
+
+        Component::macro('featureDisabled', function (string $featureFlag): static {
+            $this->isVisible = Features::disabled($featureFlag);
+
+            return $this;
+        });
+
+        Column::macro('featureEnabled', function (string $featureFlag): static {
+            $this->isVisible = Features::enabled($featureFlag);
+
+            return $this;
+        });
+
+        Column::macro('featureDisabled', function (string $featureFlag): static {
+            $this->isVisible = Features::disabled($featureFlag);
+
+            return $this;
+        });
+    }
 }
