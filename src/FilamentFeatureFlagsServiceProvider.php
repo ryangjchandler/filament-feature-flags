32ab52b5bbd360f2d80cd245fbc8eade6bbf8a44
Sebastiaan Kloos <sebastiaan@codecycler.com>
2022-04-29 12:30:38 +0200

    Add support for closures

--- src/FilamentFeatureFlagsServiceProvider.php
+++ src/FilamentFeatureFlagsServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace RyanChandler\FilamentFeatureFlags;
 
+use Closure;
 use Filament\PluginServiceProvider;
 use Filament\Tables\Columns\Column;
 use RyanChandler\FilamentFeatureFlags\Resources\FeatureFlagResource;
 use RyanChandler\LaravelFeatureFlags\Facades\Features;
 use Filament\Forms\Components\Component;
+use RyanChandler\LaravelFeatureFlags\Models\Contracts\HasFeatures;
 
 class FilamentFeatureFlagsServiceProvider extends PluginServiceProvider
 {
@@ -18,26 +20,34 @@ class FilamentFeatureFlagsServiceProvider extends PluginServiceProvider
 
     protected function registerMacros(): void
     {
-        Component::macro('featureEnabled', function (string $featureFlag): static {
-            $this->isVisible = Features::enabled($featureFlag);
+        Component::macro('featureEnabled', function (string $name, HasFeatures|Closure $for = null): static {
+            $this->visible(function () use ($name, $for) {
+                return Features::enabled($name, for: $this->evaluate($for));
+            });
 
             return $this;
         });
 
-        Component::macro('featureDisabled', function (string $featureFlag): static {
-            $this->isVisible = Features::disabled($featureFlag);
+        Component::macro('featureDisabled', function (string $featureFlag, HasFeatures|Closure $for = null): static {
+            $this->visible(function () use ($name, $for) {
+                return Features::disabled($name, for: $this->evaluate($for));
+            });
 
             return $this;
         });
 
-        Column::macro('featureEnabled', function (string $featureFlag): static {
-            $this->isVisible = Features::enabled($featureFlag);
+        Column::macro('featureEnabled', function (string $featureFlag, HasFeatures|Closure $for = null): static {
+            $this->visible(function () use ($name, $for) {
+                return Features::enabled($name, for: $this->evaluate($for));
+            });
 
             return $this;
         });
 
-        Column::macro('featureDisabled', function (string $featureFlag): static {
-            $this->isVisible = Features::disabled($featureFlag);
+        Column::macro('featureDisabled', function (string $featureFlag, HasFeatures|Closure $for = null): static {
+            $this->visible(function () use ($name, $for) {
+                return Features::disabled($name, for: $this->evaluate($for));
+            });
 
             return $this;
         });
